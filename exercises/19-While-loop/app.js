013fdc5f7f3808aebd380ac249e41f69e944a59e
DC0507 <dcalderon179105@gmail.com>
2021-02-22 21:24:13 +0000

    Exercises 1-20 finished

--- exercises/19-While-loop/app.js
+++ exercises/19-While-loop/app.js
@@ -3,13 +3,13 @@
 function startCounting()
 {
 	var counter = 100;
-	while(counter <= 100)
+	while(counter > 0)
 	{
 		counter --;
 		console.log(counter);
 	}
 	
-	return counter;
+	console.log(counter);
 }
 
 startCounting();
--- exercises/20-Random-colors-loop/app.js
+++ exercises/20-Random-colors-loop/app.js
@@ -17,9 +17,15 @@ function getColor(colorNumber=0)
 }
 
 function getAllStudentColors(){
-	
-	//your loop here
-	var exampleColor = getColor(1);
+    var studentsColors = [];
+    var randNum = Math.floor(Math.random()*4 + 1);
+    //your loop here
+    for (var i=1; i<11; i++){
+        var color = getColor(randNum);
+        studentsColors.push(color);
+        console.log(color);
+    }
+	return studentsColors;
 }
 
 //call the function below with the number of students in the class and print on the console
